40cf791326957e3aea7cb4a7bf812ec4ba736795
Liliana Weimer <lilianaweimer11@gmail.com>
2020-03-21 09:40:40 -0600

    Add code so all part.js tests pass

---
+++ src/part.js
@@ -0,0 +1,38 @@
+var validTypes = [
+  "shell",
+  "hyperdrive",
+  "computer",
+  "life support",
+  "landing gear",
+  undefined
+];
+
+class Part {
+  constructor(obj) {
+    this.name = obj.name;
+    this.type = (
+      obj.type === validTypes[0] ||
+      obj.type === validTypes[1] ||
+      obj.type === validTypes[2] ||
+      obj.type === validTypes[3] ||
+      obj.type === validTypes[4] ? obj.type : undefined
+    );
+    this.value = obj.value;
+    this.broken = false;
+  }
+  isValid() {
+    if (!this.name || !this.type || !this.value) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+module.exports = Part;
+
+
+// isValid(obj) {
+//   (this.type = validTypes) ? true : false;
+//   return this.isValid;
+// }
